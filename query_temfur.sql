317c437e4ef01979744f8c7413151fa768fbfe05
haryandrafatwa <haryandrafatwa010@gmail.com>
2019-09-02 05:46:28 +0700

    Add Query Select

---
+++ query_temfur.sql
@@ -0,0 +1,5 @@
+Select nama_penjual from penjual where nama_penjual LIKE 'A%';
+Select * FROM barang ORDER BY harga_barang DESC;
+Select no_ktp_pembeli where nama_pembeli = "Ana";
+Select * suplai;
+Select id_transaksi from transaksi WHERE total_harga > 200000;
