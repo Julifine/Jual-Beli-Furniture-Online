0d194cf4106675352db51d6f723184f92bba58f1
haryandrafatwa <haryandrafatwa010@gmail.com>
2019-09-15 21:19:32 +0700

    Delete Unfaedah File

--- query_temfur.sql
+++
@@ -1,5 +0,0 @@
-Select nama_penjual from penjual where nama_penjual LIKE 'A%';
-Select * FROM barang ORDER BY harga_barang DESC;
-Select no_ktp_pembeli where nama_pembeli = "Ana";
-Select * suplai;
-Select id_transaksi from transaksi WHERE total_harga > 200000;
